95318aefb4971d4b2f0bee47246c0b7933094549
KharlAbban <khvngkharl123@gmail.com>
2025-01-18 15:30:51 +0000

    fix: Fix auth bug preventing user identification

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const User = require("./models/user.model");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoUri = process.env.MONGODB_CONNECTION_STRING;
+const mongoUri = process.env.MONGODB_LOCAL_CONNECTION_STRING;
 
 // Middleware
 app.use(cors());
@@ -20,14 +20,17 @@ app.use(express.urlencoded({extended: true}));
 
 // Routes
 app.use("/api/products", productsRouter);
-app.use("/auth", authRouter);
+app.use("/api/auth", authRouter);
 
-app.listen(PORT, (req, res) => {
-    console.log(`Server running on port ${PORT}`)
-});
 
 // Database connection
-mongoose.connect(mongoUri).then(() => console.log("Connected to MongoDB"))
+mongoose.connect(mongoUri)
+    .then(() => {
+        console.log("Connected to MongoDB!");
+        app.listen(PORT, (req, res) => {
+            console.log(`Server running on port ${PORT}`)
+        });
+    })
    .catch((err) => console.error("Could not connect to MongoDB", err));
 
 app.get("/", async (req, res) => {
